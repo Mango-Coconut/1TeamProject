d439b808f5260f0f04a764f1a7ced9816ea6b083
hgkang2 <hgkang2hank@naver.com>
2025-11-15 16:42:40 +0900

    플레이어 움직임
    왼쪽 오른쪽 점프 움직임

--- Assets/0. Script/Player/Player.cs
+++ Assets/0. Script/Player/Player.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float jumpForce;
+    public float moveSpeed;
+    public Rigidbody2D Rigidbody2D;
+    private bool isGrounded = true;
+
     void Start()
     {
-        
+        if (Rigidbody2D == null)
+        {
+            Rigidbody2D = GetComponent<Rigidbody2D>();
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false; 
+        }
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Rigidbody2D.linearVelocity = new Vector2(horizontalInput * moveSpeed, Rigidbody2D.linearVelocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
     }
 }
