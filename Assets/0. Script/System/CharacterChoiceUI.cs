5af5ea9ce72c337aa5f4d1c5457409a834ebe793
EhyunHwaNyeong <39735663+Mango-Coconut@users.noreply.github.com>
2025-11-17 17:13:41 +0900

    캐릭터 선택화면 1.2v
    여러 시안중 합의한것 하나로 정리 및 리팩토링

---
+++ Assets/0. Script/System/CharacterChoiceUI.cs
@@ -0,0 +1,39 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterChoiceUI : MonoBehaviour
+{
+    CharacterChoicePanel[] characterChoicePanels;
+    [SerializeField] Transform selectPanel;
+    [SerializeField] TMP_Text characterNameText;
+    
+    [Header("이동할 씬 이름")]
+    public string stageSceneName = "Stage1";
+
+    void Awake()
+    {
+        characterChoicePanels = GetComponentsInChildren<CharacterChoicePanel>();
+        for(int i=0; i<characterChoicePanels.Length; i++)
+        {
+            characterChoicePanels[i].Clicked += ShowSelectPanel;
+        }
+        HideSelectPanel();
+    }
+
+    public void ShowSelectPanel(int characterId)
+    {
+        SelectedCharacter.CurCharacter = (CharacterId)characterId;
+        characterNameText.text = SelectedCharacter.CurCharacter.ToString();
+
+        selectPanel.gameObject.SetActive(true);
+    }
+    public void HideSelectPanel()
+    {
+        selectPanel.gameObject.SetActive(false);
+    }
+    public void OnStartButtonClick()
+    {
+        SceneManager.LoadScene(stageSceneName);
+    }
+}
