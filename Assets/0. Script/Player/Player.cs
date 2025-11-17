595074f4d765deef8cd8b9d8094f98d4394f9b3e
hgkang2 <hgkang2hank@naver.com>
2025-11-17 17:52:01 +0900

    캐릭터 이동 수정

--- Assets/0. Script/Player/Player.cs
+++ Assets/0. Script/Player/Player.cs
@@ -1,11 +1,18 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float maxHP;
+    public float curHP;
+    //public float exp;
     public float jumpForce;
     public float moveSpeed;
     public Rigidbody2D Rigidbody2D;
     private bool isGrounded = true;
+
+    public event Action<float> ChangedHP;
+    //public event Action<float> ChangedExp;
 
     void Start()
     {
@@ -17,20 +24,40 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        Move();
+    }
 
+        private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+
+    void Move()
+    {
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             Rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isGrounded = false; 
         }
+                
+        float horizontalMovement = 0f;
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-
-        Rigidbody2D.linearVelocity = new Vector2(horizontalInput * moveSpeed, Rigidbody2D.linearVelocity.y);
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            horizontalMovement = 1f;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            horizontalMovement = -1f;
+        }
+        Rigidbody2D.linearVelocity = new Vector2(horizontalMovement * moveSpeed, Rigidbody2D.linearVelocity.y);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    public void ChangeHP(float value)
     {
-        isGrounded = true;
+        curHP += value;
+        
+        ChangedHP?.Invoke(curHP);
     }
+
 }
